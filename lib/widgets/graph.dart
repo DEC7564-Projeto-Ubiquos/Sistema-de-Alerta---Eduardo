be033d6d616d3bbcfaae7d89ee690df13476cb00
Igor Enrick de Carvalho <igorenrickc@gmail.com>
2023-04-23 17:30:47 -0300

    Add graph library

--- lib/widgets/graph.dart
+++ lib/widgets/graph.dart
@@ -1,7 +1,31 @@
+import 'package:emg_app/widgets/voltage_sample_chart.dart';
 import 'package:flutter/material.dart';
 
-class Graph extends StatelessWidget {
+class Graph extends StatefulWidget {
   const Graph({super.key});
+
+  @override
+  State<Graph> createState() => _GraphState();
+}
+
+class _GraphState extends State<Graph> {
+  final List<VoltageSample> sampleData = [
+    VoltageSample(0, 100),
+    VoltageSample(1, 200),
+    VoltageSample(2, 300),
+    VoltageSample(3, 400),
+    VoltageSample(4, 200),
+    VoltageSample(5, 300),
+    VoltageSample(6, 100),
+    VoltageSample(7, 200),
+    VoltageSample(8, 300),
+    VoltageSample(9, 100),
+    VoltageSample(10, 200),
+    VoltageSample(11, 300),
+    VoltageSample(12, 100),
+    VoltageSample(13, 200),
+    VoltageSample(14, 300),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -9,15 +33,60 @@ class Graph extends StatelessWidget {
       padding: const EdgeInsets.fromLTRB(24, 0, 16, 0),
       child: Row(
         children: [
-          Container(
-            color: const Color.fromARGB(25, 98, 0, 238),
+          SizedBox(
             height: 300,
             width: 100,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FilledButton(
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.question_mark,
+                      size: 16,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FilledButton(
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.question_mark,
+                      size: 16,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FilledButton(
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.question_mark,
+                      size: 16,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FilledButton(
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.question_mark,
+                      size: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-          Container(
-            color: const Color.fromARGB(50, 98, 0, 238),
+          SizedBox(
             height: 300,
             width: (MediaQuery.of(context).size.width * 0.75) - 140,
+            child: VoltageSampleChart(data: sampleData),
           ),
         ],
       ),
---
+++ lib/widgets/voltage_sample_chart.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class VoltageSample {
+  final double sample;
+  final double voltage;
+
+  VoltageSample(this.sample, this.voltage);
+}
+
+class VoltageSampleChart extends StatelessWidget {
+  final List<VoltageSample> data;
+
+  const VoltageSampleChart({Key? key, required this.data}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SfCartesianChart(
+      primaryXAxis: NumericAxis(
+        title: AxisTitle(text: 'Amostra (kHz)'),
+        interval: 1,
+        majorGridLines: MajorGridLines(width: 0),
+      ),
+      primaryYAxis: NumericAxis(
+        title: AxisTitle(text: 'Tensão (mV)'),
+        interval: 100,
+        majorGridLines: MajorGridLines(width: 0),
+      ),
+      series: <ChartSeries>[
+        SplineSeries<VoltageSample, double>(
+          dataSource: data,
+          xValueMapper: (VoltageSample voltageSample, _) =>
+              voltageSample.sample,
+          yValueMapper: (VoltageSample voltageSample, _) =>
+              voltageSample.voltage,
+          width: 2,
+          splineType: SplineType
+              .cardinal, // Adicionar esta linha para tornar a linha curva
+        ),
+      ],
+      zoomPanBehavior: ZoomPanBehavior(
+        enablePinching: true,
+        zoomMode: ZoomMode.x,
+        enablePanning: true,
+      ),
+    );
+  }
+}
